9285ec3b3d178e1a8f71f719fea25d0388711b48
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-16 17:00:29 -0300

    Componente Time parte 2

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,11 +1,10 @@
 import './Time.css'
 
 export const Time = (props) => {
-
     return (
-        <div className="time">
+        <div className="time" style={ {backgroundColor: props.corSecundaria} }>
             <section>
-                <h3>{props.nome}</h3>
+                <h3 style={ {borderColor: props.corPrimaria} }>{props.nome}</h3>
             </section>
         </div>
     )
