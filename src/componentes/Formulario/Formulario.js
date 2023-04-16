99599092dd9556ade33b2caf80102dbebc0f7a67
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-16 14:30:43 -0300

    Ouvindo eventos e campos obrigatorios

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -15,14 +15,19 @@ export const Formulario = () => {
         'Inovacao e Gestão'
     ]
     
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <ListaSuspensa label="Time" itens={times}></ListaSuspensa>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}></ListaSuspensa>
                 <Botao>
                     Criar Card
                 </Botao>
