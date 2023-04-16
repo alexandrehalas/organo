c01e469d4d6b8865d8aa29c2d2a21bd17a377a3c
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-16 16:40:53 -0300

    Componente Time inicio

---
+++ src/componentes/Time/Time.js
@@ -0,0 +1,12 @@
+import './Time.css'
+
+export const Time = (props) => {
+
+    return (
+        <div className="time">
+            <section>
+                <h3>{props.nome}</h3>
+            </section>
+        </div>
+    )
+}
