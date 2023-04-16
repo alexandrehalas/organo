c8e11676df2b967777f867a50e71ed04a011538f
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-16 14:20:10 -0300

    Componente Botao

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -22,6 +23,9 @@ export const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <ListaSuspensa label="Time" itens={times}></ListaSuspensa>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
